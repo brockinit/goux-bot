bfd78df67e967afe57e38db6170160a639d47d7d
Brock Lanoza <brock@digit.co>
2019-01-03 20:21:32 -0800

    Add script to collect all messages

--- config.js
+++ config.js
@@ -1,9 +1,20 @@
 module.exports = {
+  GROUP_ME_USERS: {
+    BEN: '10729213',
+    MIC: '10720109',
+    MIKE: '10720105',
+    ANT: '10720103',
+    BROCK: '10720108',
+    SAL: '14954976',
+    GEORGE: '10720104',
+    DALTON: '15007302',
+  },
   GROUP_ME_API: {
     BOT_ID: 'd4fb7af210bd3e1bfc39be2a99',
     HOST: 'https://api.groupme.com/v3',
     BOT_MESSAGE: '/bots/post',
     GET_MESSAGES: '/groups/4601875/messages',
+    TOKEN: process.env.GROUP_ME_TOKEN,
   },
   GIPHY_API: 'https://api.giphy.com/v1/gifs/',
 };
--- utils/messages.js
+++ utils/messages.js
@@ -1,10 +1,45 @@
 const request = require('request-promise-native');
-const { GROUP_ME_API } = require('./config');
+const { GROUP_ME_API } = require('../config');
 
-async function getMessages(cursor = null) {
+let beforeId;
 
+async function getMessages() {
+  let res;
+  const options = {
+    headers: {
+      'X-Access-Token': GROUP_ME_API.TOKEN,
+    },
+    uri: `${GROUP_ME_API.HOST}${GROUP_ME_API.GET_MESSAGES}?limit=100`,
+    json: true,
+  };
+  if (beforeId) {
+    options.uri += `&before_id=${beforeId}`;
+  }
+  try {
+    res = await request.get(options);
+  } catch (err) {
+    console.error(`Error getting messages: ${err.message}`);
+    return;
+  }
+  return res;
 }
 
-module.exports = {
+async function getMoreMessages() {
+  let res;
+  try {
+    res = await getMessages();
+  } catch (err) {
+    console.error(`Error getMessages: ${err.message}`);
+  }
+  // No more messages
+  if (res.meta.code === 304) {
+    console.log('Done!!');
+    return;
+  }
+  const { messages } = res.response;
+  console.log(messages, 'messages');
+  beforeId = messages[messages.length - 1].id;
+  return getMoreMessages();
+}
 
-};
+getMoreMessages();
